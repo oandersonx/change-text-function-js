d4631878a15561ec162c110c5a32d5f3c4b151c6
Anderson <anderson.jribeiro9@gmail.com>
2021-07-02 04:48:53 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function alterar(){
+    document.getElementById('primeiro').innerHTML = 'alterado'; // Tanto faz um ou outro
+    document.querySelector('#segundo').innerHTML = 'alterado';
+    document.querySelector("#btn_send").innerHTML = "alterado";
+    document.getElementById("txt_escreva_aqui").value = 'alterado';
+}
